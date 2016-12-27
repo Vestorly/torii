41a3988c52a04c9a5328748fb3ebc5590385af96
Jim Durand <me@jdurand.com>
2016-12-27 18:28:48 +0000

    Fix test('session is injectable using inject.service’) for ember >=2.10.0

--- tests/acceptance/ember-init-test.js
+++ tests/acceptance/ember-init-test.js
@@ -60,7 +60,15 @@ test('session is injectable using inject.service', function(assert){
     torii: Ember.inject.service('torii')
   }));
 
-  var component = lookupFactory(app, 'component:testComponent').create();
+  var component;
+
+  try {
+    component = lookupFactory(app, 'component:testComponent').create({
+      renderer: lookupFactory(app, 'renderer:-inert')
+    });
+  } catch(e) {
+    component = lookupFactory(app, 'component:testComponent').create();
+  }
 
   assert.ok(component.get('session'), 'Component has access to injected session service');
   assert.ok(component.get('torii'), 'Component has access to injected torii service');
