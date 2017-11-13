83fbd7e73a1aa9be09d0c26f100a465b028d490e
Yoran Brondsema <yoran@sutori.com>
2017-11-13 09:33:40 -0600

    Refactor oauth2-code-test

--- addon/lib/required-property.js
+++ addon/lib/required-property.js
@@ -1,6 +1,6 @@
 function requiredProperty(){
   return Ember.computed(function(key){
-    throw new Error("Definition of property "+key+" by a subclass is required.");
+    throw new Error(`Definition of property ${key} by a subclass is required.`);
   });
 }
 
--- tests/unit/providers/oauth2-code-test.js
+++ tests/unit/providers/oauth2-code-test.js
@@ -1,20 +1,21 @@
 import { getConfiguration, configure } from 'torii/configuration';
 import BaseProvider from 'torii/providers/oauth2-code';
 import QUnit from 'qunit';
+import sinon from 'sinon';
 
 let { module, test } = QUnit;
 let provider;
 let tokenProvider;
 let originalConfiguration;
 
-var Provider = BaseProvider.extend({
+const Provider = BaseProvider.extend({
   name: 'mock-oauth2',
   baseUrl: 'http://example.com',
   redirectUri: 'http://foo',
   responseParams: ['state', 'authorization_code'],
 });
 
-var TokenProvider = BaseProvider.extend({
+const TokenProvider = BaseProvider.extend({
   name: 'mock-oauth2-token',
   baseUrl: 'http://example.com',
   redirectUri: 'http://foo',
@@ -40,21 +41,30 @@ module('Unit | Provider | MockOauth2Provider (oauth2-code subclass)', {
   }
 });
 
-test("BaseProvider subclass must have baseUrl", function(assert){
-  var Subclass = BaseProvider.extend();
-  var provider = Subclass.create();
-  assert.throws(function(){
-    provider.buildUrl();
-  }, /Definition of property baseUrl by a subclass is required./);
+test("BaseProvider subclass must have baseUrl", function(assert) {
+  const Subclass = BaseProvider.extend();
+  const provider = Subclass.create({
+    name: 'mock-oauth2-code'
+  });
+
+  assert.throws(
+    function() {
+      provider.open();
+    },
+    /Definition of property baseUrl by a subclass is required./
+  );
 });
 
-test("Provider requires an apiKey", function(assert){
-  assert.throws(function(){
-    provider.buildUrl();
-  }, /Expected configuration value apiKey to be defined.*mock-oauth2/);
+test("Provider requires an apiKey", function(assert) {
+  assert.throws(
+    function(){
+      provider.open();
+    },
+    /Expected configuration value apiKey to be defined.*mock-oauth2/
+  );
 });
 
-test("Provider generates a URL with required config", function(assert){
+test("Provider generates a URL with required config", function(assert) {
   configure({
     providers: {
       'mock-oauth2': {
@@ -62,12 +72,23 @@ test("Provider generates a URL with required config", function(assert){
       }
     }
   });
-  var state = provider.get('state');
-  assert.equal(provider.buildUrl(), 'http://example.com?response_type=code&client_id=dummyKey&redirect_uri=http%3A%2F%2Ffoo&state=' + state,
-        'generates the correct URL');
+  const state = provider.get('state');
+  assert.equal(
+    provider.buildUrl(),
+    `http://example.com?response_type=code&client_id=dummyKey&redirect_uri=http%3A%2F%2Ffoo&state=${state}`,
+    'generates the correct URL'
+  );
 });
 
-test("Provider generates a URL with optional scope", function(assert){
+test("Provider#open generates a URL with optional scope", function(assert) {
+  const Subclass = BaseProvider.extend({
+    name: 'mock-oauth2',
+    baseUrl: 'http://example.com',
+    responseParams: [],
+    redirectUri: 'http://foo'
+  });
+  const provider = Subclass.create();
+
   configure({
     providers: {
       'mock-oauth2': {
@@ -76,13 +97,26 @@ test("Provider generates a URL with optional scope", function(assert){
       }
     }
   });
-  var state = provider.get('state');
-  assert.equal(provider.buildUrl(), 'http://example.com?response_type=code&client_id=dummyKey&redirect_uri=http%3A%2F%2Ffoo&state=' + state + '&scope=someScope',
-        'generates the correct URL');
+
+  const state = provider.get('state');
+
+  const open = sinon.stub().resolves({});
+  provider.set('popup', {
+    open: open
+  });
+
+  provider.open();
+
+  assert.ok(
+    open.calledWith(
+      `http://example.com?response_type=code&client_id=dummyKey&redirect_uri=http%3A%2F%2Ffoo&state=${state}&scope=someScope`,
+    ),
+    'generates the correct URL'
+  );
 });
 
-test('Provider#open assert.throws when any required response params are missing', function(assert){
-  assert.expect(3);
+test('Provider#open assert.throws when any required response params are missing', async function(assert) {
+  assert.expect(2);
 
   configure({
     providers: {
@@ -93,28 +127,27 @@ test('Provider#open assert.throws when any required response params are missing'
     }
   });
 
-  var mockPopup = {
-    open: function(/*url, responseParams*/){
-      assert.ok(true, 'calls popup.open');
-
-      return Ember.RSVP.resolve({state: 'state'});
-    }
-  };
-
+  const mockPopup = {
+    open: sinon.stub().resolves({
+      state: 'state'
+    })
+  }
   provider.set('popup', mockPopup);
 
-  Ember.run(function(){
-    provider.open().then(function(){
-      assert.ok(false, '#open should not resolve');
-    }).catch(function(e){
-      assert.ok(true, 'failed');
-      var message = e.toString().split('\n')[0];
-      assert.equal(message, 'Error: The response from the provider is missing these required response params: authorization_code');
-    });
-  });
+  try {
+    await provider.open();
+  } catch (e) {
+    assert.ok(mockPopup.open.called, 'calls popup.open');
+
+    const message = e.toString().split('\n')[0];
+    assert.equal(
+      message,
+      'Error: The response from the provider is missing these required response params: authorization_code'
+    );
+  }
 });
 
-test('should use the value of provider.responseType as key for the authorizationCode', function(assert){
+test('should use the value of provider.responseType as key for the authorizationCode', async function(assert) {
   assert.expect(2);
 
   configure({
@@ -127,39 +160,47 @@ test('should use the value of provider.responseType as key for the authorization
     }
   });
 
-  var mockPopup = {
-    open: function(/*url, responseParams*/){
-      assert.ok(true, 'calls popup.open');
-      return Ember.RSVP.resolve({ 'token_id': 'test', 'authorization_code': 'pief', 'state': 'test-state' });
-    }
-  };
-
+  const mockPopup = {
+    open: sinon.stub().resolves({
+      token_id: 'test',
+      authorization_code: 'pief',
+      state: 'test-state'
+    })
+  }
   tokenProvider.set('popup', mockPopup);
 
-  Ember.run(function(){
-    tokenProvider.open().then(function(res){
-      assert.ok(res.authorizationCode === 'test', 'authenticationToken present');
-    });
-  });
+  const res = await tokenProvider.open();
+
+  assert.ok(mockPopup.open.called, 'calls popup.open');
+  assert.equal(
+    res.authorizationCode, 'test',
+    'authenticationToken present'
+  );
 });
 
-test('provider generates a random state parameter', function(assert){
+test('provider generates a random state parameter', function(assert) {
   assert.expect(1);
 
-  var state = provider.get('state');
+  const state = provider.get('state');
 
-  assert.ok(/^[A-Za-z0-9]{16}$/.test(state), 'state is 16 random characters');
+  assert.ok(
+    /^[A-Za-z0-9]{16}$/.test(state),
+    'state is 16 random characters'
+  );
 });
 
-test('provider caches the generated random state', function(assert){
+test('provider caches the generated random state', function(assert) {
   assert.expect(1);
 
-  var state = provider.get('state');
+  const state = provider.get('state');
 
-  assert.equal(provider.get('state'), state, 'random state value is cached');
+  assert.equal(
+    provider.get('state'), state,
+    'random state value is cached'
+  );
 });
 
-test('can override state property', function(assert){
+test('can override state property', function(assert) {
   assert.expect(1);
 
   configure({
@@ -170,13 +211,15 @@ test('can override state property', function(assert){
     }
   });
 
-  var state = provider.get('state');
+  const state = provider.get('state');
 
-  assert.equal(state, 'insecure-fixed-state',
-        'specified state property is set');
+  assert.equal(
+    state, 'insecure-fixed-state',
+    'specified state property is set'
+  );
 });
 
-test('URI-decodes the authorization code', function(assert){
+test('URI-decodes the authorization code', async function(assert) {
   assert.expect(1);
 
   configure({
@@ -189,21 +232,19 @@ test('URI-decodes the authorization code', function(assert){
     }
   });
 
-  var mockPopup = {
-    open: function(/*url, responseParams*/){
-      return Ember.RSVP.resolve({
-        'token_id': encodeURIComponent('test=='),
-        'authorization_code': 'pief',
-        'state': 'test-state'
-      });
-    }
-  };
-
+  const mockPopup = {
+    open: sinon.stub().resolves({
+      token_id: encodeURIComponent('test=='),
+      authorization_code: 'pief',
+      state: 'test-state'
+    })
+  }
   tokenProvider.set('popup', mockPopup);
 
-  Ember.run(function(){
-    tokenProvider.open().then(function(res){
-      assert.equal(res.authorizationCode, 'test==', 'authorizationCode decoded');
-    });
-  });
+  const res = await tokenProvider.open();
+
+  assert.equal(
+    res.authorizationCode, 'test==',
+    'authorizationCode decoded'
+  );
 });
